452c0065b8e1b84f3989cf4659c257c385ecdee1
Jeff Babio <babiojeff@gmail.com>
2018-12-12 14:06:36 -0500

    Gemstore intial commit

---
+++ src/views/checkout/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './index.css';
+
+class Checkout extends Component {
+  render() {
+    return (
+      <div className="Checkout">
+      <h1>Checkout page</h1>
+
+      </div>
+    );
+  }
+}
+
+export default Checkout;
